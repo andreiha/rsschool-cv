0f4b977851ef7ef53963fba9d0345d01c331d245
andreiha <z.karolin@gmail.com>
2020-12-12 16:52:21 +0300

    feat: basic layout

---
+++ meditation-app/app.js
@@ -0,0 +1,14 @@
+const app = () => {
+    const song = document.querySelector('.song'),
+        play = document.querySelector('.play'),
+        outline = document.querySelector('.moving-outline circle'),
+        video = document.querySelector('.vid-container video'),
+        sounds = document.querySelectorAll('.sound-picker button'),
+        timeDisplay = document.querySelector('.time-display'),
+        outlineLength= outline.getTotalLenght(),
+
+    let fakeDuration = 600;
+    
+
+        
+}
