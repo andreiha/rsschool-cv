61774a47ff5d312497c6918f6c4fa2eaedf6efc7
andreiha <z.karolin@gmail.com>
2021-01-05 00:15:50 +0300

    feat: add customize buttons

--- canvas/script.js
+++ canvas/script.js
@@ -1,21 +1,24 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
+let colorCheckbox = document.getElementById('color_checkbox');
+let colorPicker = document.getElementById('color_picker');
+let widthCheckbox = document.getElementById('width_checkbox');
+let widthPicker = document.getElementById('width_picker');
+let clearButton = document.getElementById('clear_button');
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let color = 0;
 let direction = true;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
+canvas.width = document.documentElement.clientWidth;
+canvas.height = `${document.documentElement.clientHeight}`;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 50;
+ctx.lineWidth = 30;
 
 function draw(e) {
     if (!isDrawing) return;
-    console.log(e);
     chooseColor();
     chooseWidth();
     ctx.beginPath();
@@ -27,32 +30,60 @@ function draw(e) {
 }
 
 function chooseColor() {
-    ctx.strokeStyle = `hsl(${color}, 100%, 50%)`;
-    color++;
-    if (color >= 360) {
+    if (!colorCheckbox.checked) {
+        ctx.strokeStyle = colorPicker.value;
+    } else {
+        ctx.strokeStyle = `hsl(${color}, 100%, 50%)`;
+        color++;
+        if (color >= 360) {
         color = 0;
+        }
     }
 }
 
 function chooseWidth() {
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <=1) {
-        direction = !direction;
-    }
-    if (direction) {
-        ctx.lineWidth++;
+    if (!widthCheckbox.checked) {
+        ctx.lineWidth = widthPicker.value;
     } else {
-        ctx.lineWidth--;
+        if (ctx.lineWidth >= 100 || ctx.lineWidth <=1) {
+            direction = !direction;
+        }
+        if (direction) {
+            ctx.lineWidth++;
+        } else {
+            ctx.lineWidth--;
+        }
     }
 }
 
+function enableWidthPicker() {
+    if (widthCheckbox.checked) {
+        widthPicker.disabled = true;
+    } else {
+        widthPicker.disabled = false;
+    } 
+}
+
+function enableColorPicker() {
+    if (colorCheckbox.checked) {
+        colorPicker.disabled = true;
+    } else {
+        colorPicker.disabled = false;
+    }
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     lastX = e.offsetX;
     lastY = e.offsetY;
 });
-
 canvas.addEventListener('mousemove', draw);
-
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+colorCheckbox.addEventListener('click', enableColorPicker);
+widthCheckbox.addEventListener('click', enableWidthPicker);
+clearButton.addEventListener('click', clearCanvas);
