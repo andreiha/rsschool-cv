3cabc257856778842c48fa5d61249d220b927f8d
andreiha <z.karolin@gmail.com>
2021-01-06 19:00:30 +0300

    feat: speed controller

--- whack-a-mole/script.js
+++ whack-a-mole/script.js
@@ -1,12 +1,19 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
-const button = document.querySelector('.start__button');
+const startButton = document.getElementById('start__button');
+const speedButtons = document.querySelectorAll('.speed__button');
+const slowerButton = document.getElementById('slower__button');
+const fasterButton = document.getElementById('faster__button');
+const currentSpeed = document.getElementById('current-speed');
+
+
 let lastHole;
 let timeUp = false;
 let score = 0;
-let baseSpeed = 3;
-let baseQuant = 3;
+let basicSpeed = 0;
+let minspeed = 300;
+let maxspeed = 1000;
 
 
 function randomTime(min, max) {
@@ -21,10 +28,10 @@ function randomHole(holes) {
     }
     lastHole = hole;
     return hole;
-  }
+}
 
 function peep() {
-    const time = randomTime(200, 1000);
+    const time = randomTime(minspeed, maxspeed);
     const hole = randomHole(holes);
     hole.classList.add('up');
     setTimeout(() => {
@@ -42,12 +49,48 @@ function startGame() {
 }
 
 function bonk(e) {
-    console.log(e);
     score++;
     this.classList.remove('up');
     scoreBoard.textContent = score;
 }
 
+function changeSpeedSetting (e) {
+    if (e.target === fasterButton) {
+        basicSpeed++;
+    }
+    if (e.target === slowerButton) {
+        basicSpeed--;
+    }
+    basicSpeed === 2 ? fasterButton.disabled = true : fasterButton.disabled = false;
+    basicSpeed === -2 ? slowerButton.disabled = true : slowerButton.disabled = false;
+    console.log(basicSpeed);
+    changeSpeed();
+}
+
+function changeSpeed() {
+    if (basicSpeed === 2) {
+        minspeed = 200;
+        maxspeed = 400;
+    }
+    if (basicSpeed === 1) {
+        minspeed = 200;
+        maxspeed = 600;
+    }
+    if (basicSpeed === 0) {
+        minspeed = 300;
+        maxspeed = 900;
+    }
+    if (basicSpeed === -1) {
+        minspeed = 450;
+        maxspeed = 1200;
+    }
+    if (basicSpeed === -2) {
+        minspeed = 800;
+        maxspeed = 1500;
+    }
+    console.log(minspeed, maxspeed);
+}
 
 moles.forEach(mole => mole.addEventListener('click', bonk));
-button.addEventListener('click', startGame);
+startButton.addEventListener('click', startGame);
+speedButtons.forEach(button => button.addEventListener('click', changeSpeedSetting));
