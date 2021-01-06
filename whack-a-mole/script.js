23aa6e0e0a5e4668f4085e2cc7e8ee5b6f2da470
andreiha <z.karolin@gmail.com>
2021-01-06 13:56:32 +0300

    feat: basic mole layout

---
+++ whack-a-mole/script.js
@@ -0,0 +1,32 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const button = document.getElementById('button');
+let lastHole;
+
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max-min) + min);
+}
+
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole = lastHole) {
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+}
+
+
+
+
+
+button.addEventListener('click', startGame);
