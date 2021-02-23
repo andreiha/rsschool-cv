65a13013b25eed736fc34d670cf7cd535017c8b0
andreiha <z.karolin@gmail.com>
2021-02-23 12:08:29 +0300

    init: install webpack and plugins

---
+++ fancy-weather/bs-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "port": 3000,  
+    "notify": false, // не показывать в браузере чёрное окошко BrowserSync
+    "ui": false, // не открывать отдельный порт для управления BrowserSync
+    "files":  "*.none", // следить за изменением таких файлов, то есть никаких
+    "server": { "baseDir": "./dist" }
+};
